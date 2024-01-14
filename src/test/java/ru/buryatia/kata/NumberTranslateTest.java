7e417484642146ffdd483a4fe423e21f5e790ae3
leonid <kluchnik2004@mail.ru>
2024-01-14 23:37:44 +0800

    Add new Test

--- src/test/java/ru/buryatia/kata/NumberTranslateTest.java
+++ src/test/java/ru/buryatia/kata/NumberTranslateTest.java
@@ -1,5 +1,6 @@
 package ru.buryatia.kata;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,10 +10,12 @@ public class NumberTranslateTest {
 
     @Test
     public void fromRomeToArabic() {
+        Assert.assertEquals(9,NumberTranslate.fromRomeToArabic("IX"));
 
     }
 
     @Test
     public void fromArabicToRome() {
+        Assert.assertEquals("VIII", NumberTranslate.fromArabicToRome(8));
     }
 }
