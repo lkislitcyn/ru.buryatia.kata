b790860400ea7ac95495cb7b8824c347ed6356a9
leonid <kluchnik2004@mail.ru>
2024-01-15 14:43:53 +0800

    Kata

--- src/test/java/ru/buryatia/kata/CheckNumberTest.java
+++ src/test/java/ru/buryatia/kata/CheckNumberTest.java
@@ -1,21 +1,25 @@
 package ru.buryatia.kata;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
-
 public class CheckNumberTest {
+    CheckNumber checkNumber;
+
+    @Before
+    public void initClass() {
+        checkNumber = new CheckNumber();
+    }
 
     @Test
     public void checkRomanNumerals() {
-        CheckNumber checkNumber = new CheckNumber();
         Assert.assertTrue(checkNumber.checkArabianNumerals("7"));
     }
 
     @Test
     public void checkArabianNumerals() {
-        CheckNumber checkNumber = new CheckNumber();
         Assert.assertTrue(checkNumber.checkRomanNumerals("VII"));
     }
 }
